dd9d618d3508551e3ca47280bf4a804abe04c77a
Vasko <vasko5petrov@gmail.com>
2018-08-10 09:18:23 +0300

    using fetch request

--- script.js
+++ script.js
@@ -5,35 +5,50 @@ var api_key = "6811df7bd50dc17a73603f7e5d86dad0",
  	weatherImgEl = document.getElementById('weather-img');
 
 // --- XML HTTP Async Request ---
-    var http = new XMLHttpRequest();
+    // var http = new XMLHttpRequest();
 
-    http.onreadystatechange = function() {
-        if(http.status == 200 && http.readyState == 4) {
+    // http.onreadystatechange = function() {
+    //     if(http.status == 200 && http.readyState == 4) {
 
-            http.onreadystatechange = function() {
-		        if(http.status == 200 && http.readyState == 4) {
-		            var data = JSON.parse(http.response);
+    //         http.onreadystatechange = function() {
+		  //       if(http.status == 200 && http.readyState == 4) {
+		  //           var data = JSON.parse(http.response);
 
-		            locationEl.innerHTML = `The weather in ${data.name}`;
-		            tempEl.innerHTML = `${data.main.temp}&deg; C`;
-		            weatherEl.innerHTML = `${data.weather[0].description}`;
-		            weatherImgEl.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
-		        }
-		    };
+		  //           locationEl.innerHTML = `The weather in ${data.name}`;
+		  //           tempEl.innerHTML = `${data.main.temp}&deg; C`;
+		  //           weatherEl.innerHTML = `${data.weather[0].description}`;
+		  //           weatherImgEl.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
+		  //       }
+		  //   };
 
 
-            var location = JSON.parse(http.response);
+    //         var location = JSON.parse(http.response);
 
-            http.open("GET", `http://api.openweathermap.org/data/2.5/weather?q=${location.city}&appid=${api_key}&units=metric`, true);
+    //         http.open("GET", `http://api.openweathermap.org/data/2.5/weather?q=${location.city}&appid=${api_key}&units=metric`, true);
 
-            http.send();
+    //         http.send();
 
-        }
-    };
+    //     }
+    // };
 
-    http.open("GET", "http://ip-api.com/json", true);
+    // http.open("GET", "http://ip-api.com/json", true);
 
-    http.send();
+    // http.send();
+
+// --- Fetch Request ---
+
+fetch('http://ip-api.com/json').then(res => {
+	return res.json();
+}).then(location => {
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location.city}&appid=${api_key}&units=metric`).then(res => {
+		return res.json();
+	}).then(data => {
+		locationEl.innerHTML = `The weather in ${data.name}`;
+		tempEl.innerHTML = `${data.main.temp}&deg; C`;
+		weatherEl.innerHTML = `${data.weather[0].description}`;
+		weatherImgEl.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
+	})
+})
 
 // --- JQuery Request ---
 	// $(document).ready(function() {
